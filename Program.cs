8e296fe0c305bf4b7be81821981dbbd98d77be1d
Jacob Larsson <jacoblasse4@gmail.com>
2025-09-29 09:48:34 +0200

    La kommentarer i Program.cs och TradeSystem.cs

--- Program.cs
+++ Program.cs
@@ -1,19 +1,37 @@
 ﻿using App;
 
+//A user needs to be able to register an account
+//A user needs to be able to log out.
+//A user needs to be able to log in.
+//A user needs to be able to upload information about the item they wish to trade.
+//A user needs to be able to browse a list of other users items.
+//A user needs to be able to request a trade for other users items.
+//A user needs to be able to browse trade requests.
+//A user needs to be able to accept a trade request.
+//A user needs to be able to deny a trade request.
+//A user needs to be able to browse completed requests.
+
+
 TradeSystem user = new TradeSystem();
 
 bool running = true;
 
 while (running)
 {
+  //Skapar variabel med en interface typ för att spara inloggnings information.
+  IUser? active_user;
 
+  //Meny val 
   Console.Write("1. Logga in\n2. Glömt lösenord?\n3. Quit\n");
 
+  //Skapar switch:case för att användaren ska kunna använda menyn.
   switch (Console.ReadLine())
   {
 
     case "1":
-      user.Login();
+      //Kallar på Login() en funktion som loggar in användaren. Ifall användaren loggar in så sparas användarens information i active_user.
+      active_user = user.Login();
+
       break;
 
     case "2":
--- TradeSystem.cs
+++ TradeSystem.cs
@@ -9,19 +9,33 @@ public class TradeSystem
     users.Add(new User("Jacob", "jake@", "pass"));
   }
 
+  //Inloggings funktion, gör så att användaren kan logga in på sitt konto.
   public IUser? Login()
   {
+    //Skapar en interface variabel som har koll på active user.
     IUser? active_user = null;
+
+    //Kollar ifall det finns någon active user, ifall det inte är någon inloggad så kör den funktionen.
     if (active_user == null)
     {
+
       Console.Write("Username: ");
+
+      //Användare skriver in sitt username
       string username = Console.ReadLine();
 
       Console.Write("Password: ");
+
+      //Användare skriver in sitt lösenord
       string password = Console.ReadLine();
 
+
+      //foreach funktion, som kollar igenom en lista av users som finns sparade.
       foreach (IUser user in users)
       {
+        //Kollar ifall det användaren skrev innan matchar med vad som finns sparat i listan
+        //ifall informationen stämmer så skickar den tillbaka informationen till det stället Login() funktionen blev kallad.
+        //ifall informationen inte matchar så skickar den tillbaka null, alltså ingenting.
         if (user.TryLogin(username, password))
           return user;
       }
